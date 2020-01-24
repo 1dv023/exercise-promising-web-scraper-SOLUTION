a3db060c9629138087e64e0633846e5993ec20ca
mtslck <mats.loock@lnu.se>
2020-01-24 12:52:42 +0100

    Updating before spring 2020

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
  * The starting point of the application.
  *
  * @author Mats Loock
- * @version 1.3.0
+ * @version 1.0.0
  */
 
 'use strict'
 
-const { readJson, writeJson } = require('fs-extra')
 const { resolve } = require('path')
-const scrape = require('./lib/scrape')
+const { readJson, writeJson } = require('fs-extra')
+const { extractLinks } = require('./lib/scrape')
 
-const fileName = resolve('data', 'links.json')
+const path = resolve('data', 'links.json')
 
 // Check the arguments.
 const args = process.argv.slice(2)
@@ -22,18 +22,20 @@ if (args.length === 0) {
 }
 
 // Merge persistent links and scraped links and write the merged links back to the persistent storage.
-(async () => {
+;(async () => {
   try {
     // Get persistent and scarped links.
-    const persistentLinksPromise = readJson(fileName)
-    const scrapedLinksPromise = scrape.extractLinks(args)
+    // (Ignore any error reading the file with JSON and return an empty array.)
+    // eslint-disable-next-line handle-callback-err
+    const persistentLinksPromise = readJson(path).catch(error => [])
+    const scrapedLinksPromise = extractLinks(args)
     const [persistentLinks, scrapedLinks] = await Promise.all([persistentLinksPromise, scrapedLinksPromise])
 
     // Add only unique links to the persistent links.
-    const persistentLinksSet = new Set(persistentLinks.concat(scrapedLinks))
+    const persistentLinksSet = new Set([...persistentLinks, ...scrapedLinks])
 
     // Write the links to JSON file.
-    await writeJson(fileName, [...persistentLinksSet].sort(), { spaces: 4 })
+    await writeJson(path, [...persistentLinksSet].sort(), { spaces: 4 })
 
     console.log('Done!')
   } catch (error) {
@@ -44,23 +46,21 @@ if (args.length === 0) {
 // // ALTERNATIVE SOLUTION - chaining promises
 
 // // Get persistent and scarped links.
-// const persistentLinksPromise = fs.readJson(fileName)
-// const scrapedLinksPromise = scrape.extractLinks(args)
+// // eslint-disable-next-line handle-callback-err
+// const persistentLinksPromise = readJson(path).catch(error => [])
+// const scrapedLinksPromise = extractLinks(args)
 
 // Promise.all([persistentLinksPromise, scrapedLinksPromise])
-//   .then(values => {
+//   .then(([persistentLinks, scrapedLinks]) => {
 //     // Add only unique links to the persistent links.
-//     const persistentLinks = values[0]
-//     const scrapedLinks = values[1]
-
-//     const persistentLinksSet = new Set(persistentLinks.concat(scrapedLinks))
+//     const persistentLinksSet = new Set([...persistentLinks, ...scrapedLinks])
 
 //     // Write the links to JSON file.
-//     return fs.writeJson(fileName, [...persistentLinksSet].sort(), {spaces: 4})
+//     return writeJson(path, [...persistentLinksSet].sort(), { spaces: 4 })
 //   })
 //   .then(value => {
 //     console.log('Done!')
 //   })
 //   .catch(error => {
-//     console.log('ERROR:', error)
+//     console.error('ERROR:', error)
 //   })
