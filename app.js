d1c091b3e3d6807de4c0205dbdcd79d955d07dd5
Mats Loock <mats.loock@lnu.se>
2020-02-14 14:18:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const args = process.argv.slice(2)
 
 if (args.length === 0) {
   console.error('ERROR: No argument(s).')
-  process.exit(0)
+  process.exit(1)
 }
 
 // Merge persistent links and scraped links and write the merged links back to the persistent storage.
